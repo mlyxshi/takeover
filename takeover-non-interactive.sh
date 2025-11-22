56c32d170d74758880291684d5939762e44a23b6
mlyxshi <mlyxdev@gmail.com>
2025-11-22 17:16:46 +0800

    update

--- takeover-non-interactive.sh
+++ takeover-non-interactive.sh
@@ -8,20 +8,19 @@ PORT=80
 cd "$TO"
 
 if [ ! -e fakeinit ]; then
-    ./busybox echo "Please compile fakeinit.c first" 
+    ./busybox echo "Please compile fakeinit.c first"
     exit 1
 fi
 
-./busybox echo "[INFO] Setting up target filesystem..." 
+./busybox echo "Setting up target filesystem..."
 ./busybox rm -f etc/mtab
 ./busybox ln -s /proc/mounts etc/mtab
 ./busybox mkdir -p old_root
 
-./busybox echo "[INFO] Mounting pseudo-filesystems..." 
+./busybox echo "Mounting pseudo-filesystems..."
 ./busybox mount -t tmpfs tmp tmp
 ./busybox mount -t proc proc proc
 ./busybox mount -t sysfs sys sys
-
 if ! ./busybox mount -t devtmpfs dev dev; then
     ./busybox mount -t tmpfs dev dev
     ./busybox cp -a /dev/* dev/
@@ -30,42 +29,40 @@ if ! ./busybox mount -t devtmpfs dev dev; then
 fi
 ./busybox mount --bind /dev/pts dev/pts
 
-./busybox echo "[INFO] Skipping TTY setup (non-interactive mode)" 
 
-./busybox echo "[INFO] Preparing new init..." 
+
+
+
+
+
+./busybox echo "Preparing init..."
 ./busybox cat >tmp/${OLD_INIT##*/} <<EOF
 #!${TO}/busybox sh
 
-# Non-interactive init
+
 cd "${TO}"
 
-./busybox echo "[INFO] Init takeover successful"
-./busybox echo "[INFO] Pivoting root..."
-
+./busybox echo "Init takeover successful"
+./busybox echo "Pivoting root..."
 ./busybox mount --make-rprivate /
 ./busybox pivot_root . old_root
-
-./busybox echo "[INFO] Chrooting and running fakeinit..."
-
+./busybox echo "Chrooting and running init..."
 exec ./busybox chroot . /fakeinit
 EOF
-
 ./busybox chmod +x tmp/${OLD_INIT##*/}
 
-./busybox echo "[INFO] Starting secondary sshd on port $PORT" 
+./busybox echo "Starting secondary sshd"
 
 ./busybox chroot . /usr/bin/ssh-keygen -A
 ./busybox chroot . /usr/sbin/sshd -p $PORT -o PermitRootLogin=yes
 
-./busybox echo "[INFO] New sshd is running." 
-./busybox echo "[INFO] About to take over init..." 
+./busybox echo "About to take over init. This script will now pause for a few seconds."
+./busybox echo "If the takeover was successful, you will see output from the new init."
+./busybox echo "You may then kill the remnants of this session and any remaining"
+./busybox echo "processes from your new SSH session, and umount the old root filesystem."
 
 ./busybox mount --bind tmp/${OLD_INIT##*/} ${OLD_INIT}
 
-./busybox echo "[INFO] Triggering telinit u" 
 telinit u
 
-./busybox echo "[INFO] Sleeping 10s for takeover..." 
 ./busybox sleep 10
-
-./busybox echo "[INFO] Done. After takeover, reconnect to port $PORT." 
